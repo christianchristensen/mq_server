63e0fbf2ca3627ad0ae6cf2150bbbe7c6c095cb8
Chris Christensen <christian.christensen@gmail.com>
2011-09-01 14:13:31 -0500

    Update PHP cookbook with AMQP install info.

--- roles/mq_server.rb
+++ roles/mq_server.rb
@@ -2,6 +2,10 @@ name "mq_server"
 description "Message Server (AMQP) setup."
 run_list(
   "recipe[php]",
-#  "recipe[php::module_amqp]",
+  "recipe[apache2]",
+  "recipe[apache2::mod_expires]",
+  "recipe[apache2::mod_php5]",
+  "recipe[apache2::mod_rewrite]",
+  "recipe[php::module_amqp]",
   "recipe[rabbitmq]"
 )
